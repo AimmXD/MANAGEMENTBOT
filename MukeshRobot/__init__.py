5624d8576400fe18927b944f5691337698514bcf
AimX <166642209+AimmXD@users.noreply.github.com>
2024-09-11 23:24:52 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -38,7 +38,7 @@ if ENV:
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
     ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
     CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    DB_URI = os.environ.get("DATABASE_URL")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://avnadmin:AVNS_hr7jmlGkArYP6uTQqDv@pg-1f89dd4b-anirbanop51-3a7a.e.aivencloud.com:22957/defaultdb?sslmode=require")
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
     EVENT_LOGS = os.environ.get("EVENT_LOGS", None)
     INFOPIC = bool(os.environ.get("INFOPIC", "True"))
